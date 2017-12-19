49c60fa6d25da9799e107a939b8b277cc2322df8
Yuliia Aleks <ms.yuliia.aleks@hotmail.com>
2017-12-19 19:33:34 +0200

    123

--- WindowsFormsApplication11/Program.cs
+++ WindowsFormsApplication11/Program.cs
@@ -14,7 +14,7 @@ namespace WindowsFormsApplication11
         [STAThread]
         static void Main()
         {
-            Application.EnableVisualStyles();
+         /*   Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
             Console.WriteLine("Dimon");
@@ -29,7 +29,7 @@ namespace WindowsFormsApplication11
             for (int i = 0; i < 10; i++)
             {
                 Console.WriteLine("Own Branch changes");
-            }
+            }*/
            
         }
     }
